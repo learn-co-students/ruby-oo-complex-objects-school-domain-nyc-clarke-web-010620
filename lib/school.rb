8e84fefb34daba3ef3aa5d0ae36c5ec9213fd8d4
Leon Jamison <leonjamison@icloud.com>
2020-01-08 17:10:54 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School 
+    def initialize(name,roster={})
+        @name = name
+        @roster = roster
+    end 
+
+    def name
+        @name
+    end 
+
+    def roster
+        @roster
+    end
+    
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+    end 
+
+    new_array = []
+
+    new_array << add_student
+
+
+end 
