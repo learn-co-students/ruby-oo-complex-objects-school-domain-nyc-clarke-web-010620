9fbdf3147d282677b6a52606633d446bbef2549f
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-09 18:41:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
-# code here!
+require "pry"
+class School
+
+  
+  
+  def initialize(roster)
+    @roster = {}
+    
+  end 
+  
+  attr_accessor :roster, :name 
+  
+  
+
+  
+  def add_student(name, grade)
+    if roster[grade] 
+      roster[grade] << name
+    else 
+      roster[grade] = []
+      roster[grade] << name
+    end 
+  end
+  
+  
+  def grade(s_grade)
+    roster[s_grade]
+  end 
+  
+  def sort 
+
+    # binding.pry
+    new_hash = {}
+    roster.each do |key, value| 
+      new_hash[key] = value.sort 
+    end 
+     
+    return new_hash
+  end 
+  
+  
+end  
